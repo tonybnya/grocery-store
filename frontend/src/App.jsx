4bea13031f7a99769db92020fd93d94b6580fcaf
tonybnya <nya.tony2010@gmail.com>
2024-08-14 18:38:15 +0100

    build: install & setup Tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline app">Hello world!</h1>;
+  return (
+    <div className="app blur-sm text-white">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
