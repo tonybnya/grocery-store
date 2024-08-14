b4e78a140297c14fae74036a98d119f8391bb37b
tonybnya <nya.tony2010@gmail.com>
2024-08-14 10:55:29 +0100

    docs: document the handling of MySQL connection

--- backend/sql_connection.py
+++ backend/sql_connection.py
@@ -1,9 +1,18 @@
+"""
+Module to handle connection with MySQL
+"""
+
 import mysql.connector
 
+# Global variable to hold MySQL connection object
 __cnx = None
 
 
 def get_sql_connection():
+    """
+    Establish and return a MySQL connection.
+    Output: a MySQL connection object
+    """
     global __cnx
 
     if __cnx is None:
