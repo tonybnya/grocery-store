ed089ac2e3b443391d2c0d0552561a4c73bf83b1
tonybnya <nya.tony2010@gmail.com>
2024-08-14 09:58:42 +0100

    chore: add some products to the database

--- backend/products.py
+++ backend/products.py
@@ -5,4 +5,12 @@ Products DAO (Data Access Object)
 import mysql.connector
 
 cnx = mysql.connector.connect(user="root", password="", host="127.0.0.1", database="gs")
+
+cursor = cnx.cursor()
+query: str = "SELECT * FROM gs.products"
+cursor.execute(query)
+
+for (product_id, name, uom_id, price_per_unit) in cursor:
+    print(product_id, name, uom_id, price_per_unit)
+
 cnx.close()
---
+++ backend/products.sql
@@ -0,0 +1,11 @@
+-- Insert some products into the database `gs`
+INSERT INTO `gs`.`products` (`product_id`, `name`, `uom_id`, `price_per_unit`) VALUES (1, 'toothpaste', 2, 1200);
+INSERT INTO `gs`.`products` (`product_id`, `name`, `uom_id`, `price_per_unit`) VALUES (2, 'rice', 1, 400);
+INSERT INTO `gs`.`products` (`product_id`, `name`, `uom_id`, `price_per_unit`) VALUES (3, 'meat', 1, 3000);
+INSERT INTO `gs`.`products` (`product_id`, `name`, `uom_id`, `price_per_unit`) VALUES (4, 'fish', 1, 3500);
+INSERT INTO `gs`.`products` (`product_id`, `name`, `uom_id`, `price_per_unit`) VALUES (5, 'broiler chicken', 2, 5000);
+INSERT INTO `gs`.`products` (`product_id`, `name`, `uom_id`, `price_per_unit`) VALUES (6, 'sugar (powder)', 1, 500);
+INSERT INTO `gs`.`products` (`product_id`, `name`, `uom_id`, `price_per_unit`) VALUES (7, 'sugar (pieces)', 2, 1000);
+INSERT INTO `gs`.`products` (`product_id`, `name`, `uom_id`, `price_per_unit`) VALUES (8, 'oil (bottle)', 2, 1000);
+INSERT INTO `gs`.`products` (`product_id`, `name`, `uom_id`, `price_per_unit`) VALUES (9, 'soap', 2, 350);
+INSERT INTO `gs`.`products` (`product_id`, `name`, `uom_id`, `price_per_unit`) VALUES (10, 'domestic gas (12kg bottle)', 2, 6500);
