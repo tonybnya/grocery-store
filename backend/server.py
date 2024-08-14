6cb2966d3bb99ff18e066a1d7b28b6b058a388ea
tonybnya <nya.tony2010@gmail.com>
2024-08-14 15:37:08 +0100

    style+fix+feat: rename some files for readability and add CORS mechanism to the GET /products endpoint

--- backend/server.py
+++ backend/server.py
@@ -3,7 +3,9 @@ Main application performing the Flask Server.
 Definition of all the endpoints of the API.
 """
 
-import products
+from typing import Dict, List, Union
+
+import dao_products
 from flask import Flask, jsonify
 from mysql.connector import MySQLConnection
 from sql_connection import get_sql_connection
@@ -26,10 +28,17 @@ def get_products():
     READ/GET all the products from the API.
     """
     # Get the list of all the products from the database
-    products: List[Dict[str, Union[int, str, float]]] = products.get_all_products(cnx)
+    products: List[Dict[str, Union[int, str, float]]] = dao_products.get_all_products(
+        cnx
+    )
 
     # Formatting the list of products into JSON
     response = jsonify(products)
+
+    # The `Access-Control-Allow-Origin header` is part of the CORS mechanism.
+    # It tells the browser which origins are allowed to access
+    # the resources on the server.
+    response.headers.add("Access-Control-Allow-Origin", "*")
 
     return response
 
