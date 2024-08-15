0b05078cd28406cf1a1b8266b9461cd30e16ecb6
tonybnya <nya.tony2010@gmail.com>
2024-08-15 14:24:45 +0100

    style: change the background of the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 
 function App() {
   return (
-    <div className="app blur-sm text-white">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="app blur-md invert brightness-150 md:filter-none">
+      <h1 className="text-3xl font-bold text-white underline">Hello world!</h1>
     </div>
   );
 }
