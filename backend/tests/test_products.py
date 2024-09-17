1a80b18d65ac6905ec9bfa8f00331130f5fca38e
tonybnya <nya.tony2010@gmail.com>
2024-09-17 08:06:26 +0100

    test: add test for getting non existing products - since the tests are running when there are some products in the db, this precise test can't passed and it's normal

--- backend/tests/test_products.py
+++ backend/tests/test_products.py
@@ -28,6 +28,18 @@ def client(app: Flask) -> FlaskClient:
     return app.test_client()
 
 
+def extract_product(products):
+    """
+    Extract a product from the list of products.
+    """
+    product = products[0]
+    assert "product_id" in product
+    assert "name" in product
+    assert "uom_id" in product
+    assert "price_per_unit" in product
+    assert "uom_name" in product
+
+
 def test_get_products(client: FlaskClient) -> None:
     """
     Test the GET /products endpoint.
@@ -51,12 +63,31 @@ def test_get_products(client: FlaskClient) -> None:
     # Since I know the structure of data
     # I check the fields of the first product (a dictionary object)
     if products:
-        product = products[0]
-        assert "product_id" in product
-        assert "name" in product
-        assert "uom_id" in product
-        assert "price_per_unit" in product
-        assert "uom_name" in product
+        extract_product(products)
+
+
+def test_get_nonexisting_products(client: FlaskClient) -> None:
+    """
+    Test the GET /products endpoint for nonexisting products (empty table in the db).
+    """
+    # Simulate a GET request to /products endpoint
+    response = client.get("/products")
+
+    # Ensure the request is failing (status code 404)
+    assert response.status_code == 404
+
+    # Ensure the response is in JSON format
+    assert response.content_type == "application/json"
+
+    # Store the response body (the error message in this case) into a variable
+    error_message = response.get_json()
+
+    # Ensure the error message contains the "error" field
+    assert "error" in error_message
+
+    # Ensure the value of the "error" field is the same
+    # as defined in the routes implementation
+    assert error_message.get("error") == "Products not found"
 
 
 def test_get_single_product(client: FlaskClient) -> None:
@@ -86,11 +117,11 @@ def test_get_single_product(client: FlaskClient) -> None:
     if product:
         assert product.get("product_id") == 1
         assert product.get("name") == "toothpaste"
-        assert product.get("price_per_unit") == 1000.0
+        assert product.get("price_per_unit") == 1200.0
         assert product.get("uom_id") == 2
 
 
-def test_get_nonexistent_product(client: FlaskClient) -> None:
+def test_get_nonexisting_product(client: FlaskClient) -> None:
     """
     Test the GET /products/{product_id} endpoint for a nonexisting product.
     """
@@ -114,4 +145,4 @@ def test_get_nonexistent_product(client: FlaskClient) -> None:
 
     # Ensure the value of the "error" field is the same
     # as defined in the routes implementation
-    assert error_message["error"] == "Product not found"
+    assert error_message.get("error") == "Product not found"
